004c2497cac3ce3501d1a6254447b2c2dbdcc33e
AC <acsimoes446@gmail.com>
2024-11-17 23:23:50 -0300

    fix: ajustando relacionamentos

--- src/models/index.js
+++ src/models/index.js
@@ -4,13 +4,12 @@ import Experiencia from "./Experiencia.js";
 import Certificado from "./Certificado.js";
 
 // Relacionamentos
-Curriculo.hasMany(Formacao, { foreignKey: "curriculoId" });
 Formacao.belongsTo(Curriculo, { foreignKey: "curriculoId" });
-
-Curriculo.hasMany(Experiencia, { foreignKey: "curriculoId" });
 Experiencia.belongsTo(Curriculo, { foreignKey: "curriculoId" });
-
-Curriculo.hasMany(Certificado, { foreignKey: "curriculoId" });
 Certificado.belongsTo(Curriculo, { foreignKey: "curriculoId" });
+
+Curriculo.hasMany(Formacao, { foreignKey: "curriculoId" });
+Curriculo.hasMany(Experiencia, { foreignKey: "curriculoId" });
+Curriculo.hasMany(Certificado, { foreignKey: "curriculoId" });
 
 export { Curriculo, Formacao, Experiencia, Certificado };
